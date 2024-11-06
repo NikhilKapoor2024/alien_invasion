f4cfeffffa60df420f3642bde797d286ea503867
Nikhil Kapoor <nkapoor.tx@gmail.com>
2024-11-06 10:36:18 -0600

    Added the following: - Behaviors for collissions (i.e., ship/alien, alien/screen, etc.) - GameStats module to manage statistics of the game - Functions that incorporate Python the Sleep module

--- ship.py
+++ ship.py
@@ -39,3 +39,9 @@ class Ship:
     def blitme(self):
         """Draw the ship at its current location"""
         self.screen.blit(self.image, self.rect)
+    
+
+    def center_ship(self):
+        """Moves ship to bottom center"""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
